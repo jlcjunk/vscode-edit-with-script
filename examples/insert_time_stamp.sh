856c81ed29d4342558b8613b6c16fdf7a3205f24
Jason Crabtree <jlcjunk@crabtreefamily.us>
2025-12-30 17:05:09 -0600

    add examples to default settings

--- examples/insert_time_stamp.sh
+++ examples/insert_time_stamp.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-# output local time, UTC time, and a hash based on time
+# output local time, MST time, UTC time, and a hash based on time
+#     cut -c -31 = resolution of .1 second (keep from beginning to 31st character), -32 = .01 second
 
 echo -n $(date +"%Y.%m.%d %H:%M:%S %Z (")
+echo -n $(TZ=MST7 date +"%H:%M:%S %Z) (")
 echo -n $(date --utc +"%H:%M:%S %Z) (")
 for i in $(echo "ibase=10; obase=36; $(date --utc +'%s%N')" | cut -c -31 | bc)
 do 
